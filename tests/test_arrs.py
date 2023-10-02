739b00bcaa25b2adfc6b3078e105b273c0808363
Ivan <proffesor.09@mail.ru>
2023-10-03 02:06:33 +0300

    добавление тестов до покрытия в 100 %

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,25 @@
+import pytest
+
 from utils import arrs
 
+@pytest.fixture
+def arrey():
+    return [1,2,3,4,5]
 
-def test_get():
-    assert arrs.get([1, 2, 3], 2, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_get(arrey):
+    assert arrs.get(arrey, 2, "test") == 3
+    assert arrs.get(arrey, 10, "test") == "test"
+
+
+
+
+
+def test_slice(arrey):
+    assert arrs.my_slice(arrey, 1, 3) == [2, 3]
+    assert arrs.my_slice(arrey, 1) == [2, 3, 4, 5]
+    assert arrs.my_slice([],1,4) == []
+    assert arrs.my_slice(arrey, -4, -1) == [2, 3, 4]
+    assert arrs.my_slice(arrey, start=-2) == [4, 5]
+    assert arrs.my_slice(arrey, end=-2) == [1, 2, 3]
